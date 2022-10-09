6f98ace71f137eec00011c0731e7d74405d27471
RagingScout95 <prakhar99rajput@gmail.com>
2022-10-09 18:55:06 +0530

    added another card

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -4,14 +4,6 @@ import CardItem from './CardItem';
 import axios from 'axios';
 
 function Cards() {
-  // var result1;
-  // const getallproducts = async () => {
-  //   const allproducts = axios.get('http://localhost:5000/ecommerce/api/v1/allproducts');
-  //   return allproducts;
-  // }
-  // const products = getallproducts().then(result => { result1 = result; return result })
-  // .catch(error => { console.error(error); throw error; });
-  // console.log(products); 
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
@@ -26,6 +18,7 @@ function Cards() {
       {post.map((x) => (
           <CardItem 
           img = {x.productimages[0].imageurl}
+          key = {x._id}
           title = {x.productname}
           price = {x.productprice}
         />
