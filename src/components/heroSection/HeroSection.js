c169b0db4c1b4a8e3e5673466ea3edf3dbb69096
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-10-13 12:03:44 +0530

    minor debug

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import '../../App.css';
-import { Button } from '../navBar/Button';
-import './HeroSection.css';
+import React from "react";
+import "../../App.css";
+import { Button } from "../navBar/Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <h1>BESTMEDS AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           Products
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
           // onClick={console.log('hey')}
         >
-          Equipment <i className='far fa-play-circle' />
+          Equipment
         </Button>
       </div>
     </div>
