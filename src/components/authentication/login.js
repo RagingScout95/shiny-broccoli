c80a83ca2c10e486d5a4cbd9f8db6c65e6880117
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-11-15 17:53:18 +0530

    login changes

--- src/components/authentication/login.js
+++ src/components/authentication/login.js
@@ -1,55 +1,63 @@
-import React from 'react';
-import './login.css';
+import React, { useContext } from "react";
+import "./login.css";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
+function login() {
+  const { setUser } = useContext(UserContext);
+  const [email_, setemail] = React.useState();
+  const [pass, setpass] = React.useState();
+  const navigate = useNavigate();
 
-function login(){
-const [post, setPost] = React.useState({});
-const [email_, setemail] = React.useState();
-const [pass, setpass] = React.useState();
-const navigate = useNavigate();
+  function setter_email(e) {
+    setemail(e.target.value);
+  }
+  function setter_pass(e) {
+    setpass(e.target.value);
+  }
 
-function setter_email(e){
-    setemail(e.target.value)
-}
-function setter_pass(e){
-    setpass(e.target.value)
-}
-
-async function dataSubmission(e){
+  async function dataSubmission(e) {
     e.preventDefault();
     // console.log(email_, pass);
-        await axios.get(`http://localhost:5000/ecommerce/api/v1/${email_}/${pass}`)
-        .then((response) => {
-            // setPost(prevres=>{return response.data});
-            // console.log(response.data.message);
-            if(response.data.message=="sccessfully LoggedIn"){
-                navigate("/");
-            }else{
-                document.querySelector('.msg').innerHTML = `<p>${response.data.message}</p>`
-            }
-        }).catch(err=> console.log(err));
-        if (!post) return null;
-}
+    await axios
+      .get(`http://localhost:5000/ecommerce/api/v1/login/${email_}/${pass}`)
+      .then((response) => {
+        // setPost(prevres=>{return response.data});
+        // console.log(response.data.message);
+        if (response.data.message === "sccessfully LoggedIn") {
+          setUser(response.data.result);
+          navigate("/");
+        } else {
+          document.querySelector(
+            ".msg"
+          ).innerHTML = `<p>${response.data.message}</p>`;
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 
-return(
-    <div className='login-body'>
-        <div className="login-box"> 
-            <h1>Login</h1>
-            <form onSubmit={dataSubmission}>
-                <label>Email</label>
-                <input type="email" onChange={setter_email}></input>
-                <label>Password</label>
-                <input type="password" onChange={setter_pass}></input>
-                <span className='msg'></span>
-                <button className='btn-login' value="Submit">Submit</button>
-            </form>
-            <p className="outside">Don't have an account? <Link to="/signup">Signup</Link></p>
-        </div>
+  return (
+    <div className="login-body">
+      <div className="login-box">
+        <h1>Login</h1>
+        <form onSubmit={dataSubmission}>
+          <label>Email</label>
+          <input type="email" onChange={setter_email}></input>
+          <label>Password</label>
+          <input type="password" onChange={setter_pass}></input>
+          <span className="msg"></span>
+          <button className="btn-login" value="Submit">
+            Submit
+          </button>
+        </form>
+        <p className="outside">
+          Don't have an account? <Link to="/signup">Signup</Link>
+        </p>
+      </div>
     </div>
-);
+  );
 }
 
 export default login;
--- src/components/navBar/Button.js
+++ src/components/navBar/Button.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import './Button.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Button.css";
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+const STYLES = ["btn--primary", "btn--outline", "btn--test"];
 
-const SIZES = ['btn--medium', 'btn--large'];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  buttonSize,
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
@@ -20,12 +19,12 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-      <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
   );
 };
